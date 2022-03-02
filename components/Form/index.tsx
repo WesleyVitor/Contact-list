2fb77755463e7581b60da84d45330070e2f2d266
WesleyVitor <wesleydemorais@outlook.com.br>
2022-03-02 14:11:36 -0300

    add: react-hook-form to contact form

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,45 +1,51 @@
 import React from "react";
 import { Button } from "../../styles/components/Button";
 import { Input, Label, ContainerInput, ContainerForm } from "./styles";
+import { useForm } from "react-hook-form";
+import { ContactProps } from "../../pages/index";
 
 const Form: React.FC = () => {
+    const { register, handleSubmit } = useForm<ContactProps>();
+
+    const FSubmit = (data: ContactProps) =>
+        console.log(`Dados:${data.username}`);
     return (
-        <ContainerForm>
+        <ContainerForm onSubmit={handleSubmit(FSubmit)}>
             <div>
                 <ContainerInput>
                     <Label>Username</Label>
-                    <Input type="text" required />
+                    <Input {...register("username")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Email</Label>
-                    <Input type="text" required />
+                    <Input {...register("email1")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Ocupação</Label>
-                    <Input type="text" required />
+                    <Input {...register("job")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Meeting</Label>
-                    <Input type="text" required />
+                    <Input {...register("meeting")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Pinterest</Label>
-                    <Input type="text" required />
+                    <Input {...register("Pinterest")} />
                 </ContainerInput>
                 <Button type="submit">Cadastrar</Button>
             </div>
             <div>
                 <ContainerInput>
                     <Label>Instagram</Label>
-                    <Input type="text" required />
+                    <Input {...register("instagram")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Twitter</Label>
-                    <Input type="text" required />
+                    <Input {...register("twitter")} />
                 </ContainerInput>
                 <ContainerInput>
                     <Label>Linkedin</Label>
-                    <Input type="text" required />
+                    <Input {...register("Linkedin")} />
                 </ContainerInput>
             </div>
         </ContainerForm>
--- components/Form/styles.ts
+++ components/Form/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ContainerForm = styled.div`
+export const ContainerForm = styled.form`
     display: flex;
     justify-content: space-between;
 `;
